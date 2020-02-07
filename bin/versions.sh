a32599d7a652a428424b2b8e2d948b6a53646331
nalbam <me@nalbam.com>
2020-02-07 19:05:59 +0900

    version

--- bin/versions.sh
+++ bin/versions.sh
@@ -3,20 +3,19 @@
 LIST=/tmp/helm-list
 
 # installed chart version
-helm list | awk '{print $1" "$9}' > ${LIST}
+cat releases/* | grep ' chart: ' | awk '{print $2}' > ${LIST}
 
-cat ${LIST} | awk '{print $2}'
+cat ${LIST}
 
 # helm 3
 # URL="https://hub.helm.sh/charts/${REPO}"
 # LATEST=$(helm search hub ${NAME} -o json | URL="${URL}" jq -r '[.[] | select(.url==env.URL)][0] | "\(.version)"')
 
-while read VAR; do
-    ARR=(${VAR})
-
-    REPO=${ARR[0]}
+while read REPO; do
 
     echo
-    helm search ${REPO} | grep "\/${REPO}"
+    helm search ${REPO} -o json | REPO="${REPO}" jq -r '[.[] | select(.Name==env.REPO)][0] | "\(.Version)"'
+
+    # helm search ${REPO} | grep "\/${REPO}"
 
 done < ${LIST}
